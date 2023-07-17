674899eb02c3b0b26bd9f9a9f817a6948a4ecc85
qhannn <qhannn@gmail.com>
2023-07-17 19:25:53 +0300

    Update App.js
    After completing the second level return the main page

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import StartPage from './StartPage';
 import GamePage from './GamePage';
-
 
 function App() {
   const [username, setUsername] = useState('');
@@ -11,7 +11,28 @@ function App() {
     setUsername(username);
   };
 
-  return username ? <GamePage username={username} /> : <StartPage onStart={startGame} />;
+  // If a username exists, navigate to the game page
+  if (username) {
+    return (
+      <Router>
+        <Navigate to="/game" />
+        <Routes>
+          <Route path="/" element={<StartPage onStart={startGame} />} />
+          <Route path="/game" element={<GamePage username={username} />} />
+        </Routes>
+      </Router>
+    );
+  }
+
+  // If a username does not exist, stay on the start page
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartPage onStart={startGame} />} />
+        <Route path="/game" element={<GamePage username={username} />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
